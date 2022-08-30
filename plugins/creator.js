c958ca4021b37fa161eff213deee286d54ad22d8
雨 ʀᴀʏʜᴀɴ サンダー <102699105+RyhnXD@users.noreply.github.com>
2022-08-31 05:22:12 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,7 @@ function handler(m) {
   
   const kontak = {
 	"displayName": 'My owner',
-	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('6283820073017@s.whatsapp.net')}\nitem1.TEL;waid=6283820073017:6283820073017\nitem1.X-ABLabel:Busy.\nURL;My Web: https://hi.rlxfly.my.id\nEMAIL;Email Owner: me@rlxfly.my.id\nORG: NOT A BOT + NO SAVE\nTEL;My number bot;waid=6287845646738:6287845646738\nEND:VCARD`
+	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('62895627514070')}\nitem1.TEL;waid=62895627514070:62895627514070\nitem1.X-ABLabel:Busy.\nURL;My Web: https://Ahsijwij\nEMAIL;Email Owner: Han\nORG: NOT A BOT + NO SAVE\nTEL;My number bot;waid=6287845646738:6287845646738\nEND:VCARD`
 }
 
 conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })
