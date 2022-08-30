b624f7b23601b28f28f5188a24d7c4bffe44e88d
雨 ʀᴀʏʜᴀɴ サンダー <102699105+RyhnXD@users.noreply.github.com>
2022-08-31 05:23:36 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,7 @@ function handler(m) {
   
   const kontak = {
 	"displayName": 'My owner',
-	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('62895627514070')}\nitem1.TEL;waid=62895627514070:62895627514070\nitem1.X-ABLabel:Busy.\nURL;My Web: https://Ahsijwij\nEMAIL;Email Owner: Han\nORG: NOT A BOT + NO SAVE\nTEL;My number bot;waid=6287845646738:6287845646738\nEND:VCARD`
+	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('62895627514070')}\nitem1.TEL;waid=62895627514070:62895627514070\nitem1.X-ABLabel:Busy.\nURL;My Web: https://Ahsijwij\nEMAIL;Email Owner: Han\nORG: NOT A BOT + NO SAVE\nTEL;My number bot;waid=6281779211748:6281779211748\nEND:VCARD`
 }
 
 conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })
