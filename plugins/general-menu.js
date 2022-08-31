907559ffa0c7034eba88f083581d791cf3f295c4
雨 ʀᴀʏʜᴀɴ サンダー <102699105+RyhnXD@users.noreply.github.com>
2022-08-31 19:35:14 +0700

    Update general-menu.js

--- plugins/general-menu.js
+++ plugins/general-menu.js
@@ -91,9 +91,9 @@ const defaultMenu = {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
-const vi = ['https://telegra.ph/file/067b2cb3312837533239c.mp4',
-'https://telegra.ph/file/e38881701692c74484a17.mp4',
-'https://telegra.ph/file/de776d34ef058b7d2ec12.mp4', 'https://telegra.ph/file/bc82653506c301b40679c.mp4',     'https://telegra.ph/file/7f10b3624991bbcee9ded.mp4', 'https://telegra.ph/file/51aa9701839dcc29066e9.mp4',     'https://telegra.ph/file/4f26132ac0296a34a45a8.mp4']
+const vi = ['https://telegra.ph/file/26dea419be3a235b7e1ef.mp4',
+'https://telegra.ph/file/08041d63d918edf10a20a.mp4',
+'https://telegra.ph/file/26dea419be3a235b7e1ef.mp4', 'https://telegra.ph/file/08041d63d918edf10a20a.mp4',     'https://telegra.ph/file/26dea419be3a235b7e1ef.mp4', 'https://telegra.ph/file/26dea419be3a235b7e1ef.mp4',     'https://telegra.ph/file/26dea419be3a235b7e1ef.mp4']
 
 var vid = vi[Math.floor(Math.random() * (vi.length))]
 
